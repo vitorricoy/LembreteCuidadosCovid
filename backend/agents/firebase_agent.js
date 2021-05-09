549bb0f557261ac461ade650e2dc0b8d93362076
vitorricoy <vitorricoy@gmail.com>
2021-05-08 23:29:58 -0300

    Atualizado post do backend

--- backend/agents/firebase_agent.js
+++ backend/agents/firebase_agent.js
@@ -1,36 +1,23 @@
-const https = require('https')
+const axios = require('axios')
 
 function enviarNotificacao(usuario, distancia) {
-    const data = JSON.stringify({
-        notification: {
-            title: "Lembrete de Cuidados",
-            body: "Você está a "+distancia+" km de casa desde que abriu o app pela última vez. Lembre-se dos cuidados contra a covid ao sair de casa.",
-            click_action: process.env.ACTION_PUSH,
-            icon: process.env.ICON_PUSH
-        },
-        to: usuario
-    });
-    
-    const options = {
-        hostname: "https://"+process.env.URL_PUSH,
-        path: process.env.PATH_PUSH,
-        method: 'POST',
-        headers: {
+    axios.post("https://"+process.env.URL_PUSH+process.env.PATH_PUSH, {
+            notification: {
+                title: "Lembrete de Cuidados",
+                body: "Você está a "+distancia+" km de casa desde que abriu o app pela última vez. Lembre-se dos cuidados contra a covid ao sair de casa.",
+                click_action: process.env.ACTION_PUSH,
+                icon: process.env.ICON_PUSH
+            },
+            to: usuario
+        }, {headers: {
             'Content-Type': 'application/json',
-            'Content-Length': data.length,
-            'Authorization': 'key='+process.env.KEY_PUSH
-        }
-    }
-    console.log(options);
-
-    const req = https.request(options);
-
-    req.on('error', error => {
-        console.error(error)
-    })
-
-    req.write(data)
-    req.end()
+            'Authorization': 'key='+process.env.KEY_PUSH 
+        }}).then(res => {
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(res)
+        }).catch(error => {
+            console.error(error)
+        });
 }
 
 module.exports = { enviarNotificacao };
